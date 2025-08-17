f12846e0e4cbbbcb8d28460bc16dbde4e939e263
TheZexquex <jakobreiser@gmx.de>
2025-08-17 13:31:00 +0200

    feat: add a cooldown util to run actions with a cooldown

---
+++ astralib-common/src/main/java/de/unknowncity/astralib/common/temporal/CooldownAction.java
@@ -0,0 +1,24 @@
+package de.unknowncity.astralib.common.temporal;
+
+import java.time.Duration;
+
+/**
+ * Util to allow actions to run with a cooldown
+ * in between executions
+ */
+public class CooldownAction {
+    private long lastExecution = 0;
+    private final Duration cooldownDuration;
+
+    public CooldownAction(Duration cooldownDuration) {
+        this.cooldownDuration = cooldownDuration;
+    }
+
+    public void execute(Runnable action) {
+        long now = System.currentTimeMillis();
+        if (lastExecution + cooldownDuration.toMillis() < now) {
+            lastExecution = now;
+            action.run();
+        }
+    }
+}
